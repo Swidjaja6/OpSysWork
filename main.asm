60206465e0a89e212c2118c43c04d54ffd389e6e
pokerfacer270 <Swidjaja270@gmail.com>
2021-01-03 23:08:59 -0800

    Swidjaja6

--- main.asm
+++ main.asm
@@ -1,61 +1,58 @@
-org 0x7c00
+[org 0x7c00]
+[bits 16]
 
-mov si, STR
-call printf
+section .text
 
-mov si, STR_T
-call printf
+  global main
 
-mov si, STR_TH
-call printf
+main:
 
+cli
+jmp 0x0000:ZeroSeg
+ZeroSeg:
+  xor ax, ax
+  mov ss, ax
+  mov ds, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov sp, main
+  cld
+sti
+
+push ax
+xor ax, ax
+int 0x13
+pop ax
+;mov si, STR
+;call printf
+
+;mov si, STR_T
+;call printf
+
+;mov si, STR_TH
+;call printf
+mov al, 1
+mov cl, 2
 call readDisk
+jmp test
 
 jmp $
 
-printf:
-  pusha
-  str_loop:
-    mov al, [si]
-    cmp al, 0
-    jne print_char
-    popa
-    ret
+%include "./printf.asm"
+%include "./readDisk.asm"
 
-  print_char:
-    mov ah, 0x0e
-    int 0x10
-    add si, 1
-    jmp str_loop
-
-readDisk:
-  pusha    	
-  mov ah, 0x02
-  mov dl, 0x80
-  mov ch, 0
-  mov dh, 0
-  mov al, 1
-  mov cl, 2
-
-  push bx
-  mov bx, 0
-  mov es, bx
-  pop bx
-  mov bx, 0x7c00 + 512
-
-  jc disk_err
-  popa
-  ret
-
-  disk_err:
-    mov si, DISK_ERR_MSG
-    call printf
-    jmp $
-
-STR: db "Loaded in 16-bit Real Mode to memory location 0x7c00.", 0x0a, 0x0d, 0
-STR_T: db "These messages use the BIOS interrupt 0x10 with the ah register sent to 0x0e.", 0x0a, 0x0d, 0
-STR_TH: db "Heraclitus test boot complete. Power off this machine and load a real OS, dummy.", 0
+;STR: db "Loaded in 16-bit Real Mode to memory location 0x7c00.", 0x0a, 0x0d, 0
+;STR_T: db "These messages use the BIOS interrupt 0x10 with the ah register sent to 0x0e.", 0x0a, 0x0d, 0
+;STR_TH: db "Heraclitus test boot complete. Power off this machine and load a real OS, dummy.", 0
 DISK_ERR_MSG: db "Error Loading Disk.", 0x0a, 0x0d, 0
+TEST_STR: db "You are in the second sector.", 0x0a, 0x0d, 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+test:
+mov si, TEST_STR
+call printf
+
+times 512 db 0
